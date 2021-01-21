c1af77b6dd1c19c41bdfff4c3c5d6d15d0e1865b
Zhao Junwang <zhjwpku@gmail.com>
2021-01-21 13:10:24 +0800

    LRUReplacer
    Signed-off-by: Zhao Junwang <zhjwpku@gmail.com>

--- 15-445/2017/sqlite-fall2017/src/buffer/lru_replacer.cpp
+++ 15-445/2017/sqlite-fall2017/src/buffer/lru_replacer.cpp
@@ -13,12 +13,27 @@ template <typename T> LRUReplacer<T>::~LRUReplacer() {}
 /*
  * Insert value into LRU
  */
-template <typename T> void LRUReplacer<T>::Insert(const T &value) {}
+template <typename T> void LRUReplacer<T>::Insert(const T &value) {
+  std::unique_lock<std::mutex> lock(mtx);
+  auto iter = umap.find(value);
+  if (iter != umap.end()) { // exists
+    lst.splice(lst.begin(), lst, iter->second);
+  } else {
+    umap[value] = lst.insert(lst.begin(), value);
+  }
+}
 
 /* If LRU is non-empty, pop the head member from LRU to argument "value", and
  * return true. If LRU is empty, return false
  */
 template <typename T> bool LRUReplacer<T>::Victim(T &value) {
+  std::unique_lock<std::mutex> lock(mtx);
+  if (umap.size() > 0) {
+    value = lst.back();
+    umap.erase(value);
+    lst.pop_back();
+    return true;
+  }
   return false;
 }
 
@@ -27,10 +42,20 @@ template <typename T> bool LRUReplacer<T>::Victim(T &value) {
  * return false
  */
 template <typename T> bool LRUReplacer<T>::Erase(const T &value) {
+  std::unique_lock<std::mutex> lock(mtx);
+  auto iter = umap.find(value);
+  if (iter != umap.end()) {
+    lst.erase(iter->second);
+    umap.erase(value);
+    return true;
+  }
   return false;
 }
 
-template <typename T> size_t LRUReplacer<T>::Size() { return 0; }
+template <typename T> size_t LRUReplacer<T>::Size() {
+  std::unique_lock<std::mutex> lock(mtx);
+  return umap.size();
+}
 
 template class LRUReplacer<Page *>;
 // test only
